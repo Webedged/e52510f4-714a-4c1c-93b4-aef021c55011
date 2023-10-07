42fa2e0f0a67dbc73ffa827088c626c6a5170808
Julian <jvlian.mueller.business@gmail.com>
2023-10-07 13:25:36 +0200

    angular init;

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'e52510f4-714a-4c1c-93b4-aef021c55011';
+}
